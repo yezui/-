a7c8197be260eda8b96c6361273265870e01dc0d
Administrator <Administrator@192.168.1.102>
2017-10-31 20:33:47 +0800

    一

---
+++ src/DataProcessing.java
@@ -0,0 +1,133 @@
+import java.sql.SQLException;
+import java.util.*;
+
+
+public  class DataProcessing {
+	private static boolean connectToDB=false;
+	static Hashtable<String, User> users;
+
+	static {
+		users = new Hashtable<String, User>();
+		users.put("jack", new Operator("jack","123","operator"));
+		users.put("rose", new Browser("rose","123","browser"));
+		users.put("kate", new Administrator("kate","123","administrator"));
+		Init();
+	}
+	//初始化connectToDB
+	public static  void Init(){
+		//这次暂时用随机数表示是否出现异常
+		if (Math.random()>0.2)
+			connectToDB = true;
+		else
+			connectToDB = false;
+		
+	}	
+	
+	public static User search(String name, String password) throws SQLException{
+		if ( !connectToDB ) 
+	        throw new SQLException( "Not Connected to Database" );
+		double ranValue=Math.random();
+		if (ranValue>0.5)
+			throw new SQLException( "Error in excecuting Query" );
+		if (users.containsKey(name)) {
+			User temp =users.get(name);
+			if ((temp.getPassword()).equals(password))
+				return temp;
+		}
+		System.out.println("用户名不存在或密码不匹配");
+		return null;
+	}
+	
+	public static Enumeration<User> getAllUser() throws SQLException{
+		if ( !connectToDB ) 
+	        throw new SQLException( "Not Connected to Database" );
+		
+		double ranValue=Math.random();
+		if (ranValue>0.5)
+			throw new SQLException( "Error in excecuting Query" );
+		Enumeration<User> e  = users.elements();
+		return e;
+	}
+	
+	
+	
+	public static boolean update(String name, String password, String role) throws SQLException{
+		User user;
+		if ( !connectToDB ) 
+	        throw new SQLException( "Not Connected to Database" );
+		
+		double ranValue=Math.random();
+		if (ranValue>0.5)
+			throw new SQLException( "Error in excecuting Update" );
+		
+		if (users.containsKey(name)) {
+			if (role.equalsIgnoreCase("administrator"))
+				user = new Administrator(name,password, role);
+			else if (role.equalsIgnoreCase("operator"))
+				user = new Operator(name,password, role);
+			else
+				user = new Browser(name,password, role);
+			users.put(name, user);
+			return true;
+		}else
+			return false;
+	}
+	
+	public static boolean insert(String name, String password, String role) throws SQLException{
+		User user;
+		if ( !connectToDB ) 
+	        throw new SQLException( "Not Connected to Database" );
+		
+		double ranValue=Math.random();
+		if (ranValue>0.5)
+			throw new SQLException( "Error in excecuting Insert" );
+		
+		if (users.containsKey(name))
+			return false;
+		else{
+			if (role.equalsIgnoreCase("administrator"))
+				user = new Administrator(name,password, role);
+			else if (role.equalsIgnoreCase("operator"))
+				user = new Operator(name,password, role);
+			else
+				user = new Browser(name,password, role);
+			users.put(name, user);
+			return true;
+		}
+	}
+	
+	public static boolean delete(String name) throws SQLException{
+		if ( !connectToDB ) 
+	        throw new SQLException( "Not Connected to Database" );
+		
+		double ranValue=Math.random();
+		if (ranValue>0.5)
+			throw new SQLException( "Error in excecuting Delete" );
+		
+		if (users.containsKey(name)){
+			users.remove(name);
+			return true;
+		}else
+			return false;
+		
+	}
+	
+	public void disconnectFromDB() {
+		if ( connectToDB ){
+			// close Statement and Connection            
+			try{
+			    if (Math.random()>0.5)
+					throw new SQLException( "Error in disconnecting DB" );
+			}catch ( SQLException sqlException ){                                            
+			    sqlException.printStackTrace();           
+			}finally{                                            
+				connectToDB = false;              
+			}                             
+		} 
+   }     
+	
+	public static void main(String[] args) {		
+
+	}
+	
+}
